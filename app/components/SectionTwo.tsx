84ae98ecf9a3e60e49278810c93027c00c2d733e
87tawanzinho <87tawanzinho@gmail.com>
2023-12-22 03:21:04 +0000

    second div

---
+++ app/components/SectionTwo.tsx
@@ -0,0 +1,41 @@
+export default function SectionTwo() {
+  return (
+    <div className="h-max bg-black text-white flex flex-col justify-center items-center gap-8 p-10 text-gray-200">
+      <p className="sm: text-3xl lg:text-7xl mt-10 text-yellow-500 font-bold">
+        Minha História
+      </p>
+      <p>
+        Minha jornada na programação é marcada por uma paixão precoce por
+        computadores, que se manifestou desde os primeiros anos da minha vida.
+        Fascinado pelo universo tecnológico, eu buscava constantemente maneiras
+        de interagir e entender o funcionamento por trás daquelas máquinas
+        intrigantes. Foi somente em 2021, entretanto, que eu descobri o mundo da
+        programação, dando início a uma incrível aventura que moldaria meu
+        percurso profissional. Naquela época, as limitações não me impediram de
+        perseguir meu interesse. Mesmo sem acesso a um computador, mergulhei de
+        cabeça na programação, utilizando apenas papel e caneta.
+      </p>
+
+      <p>
+        {" "}
+        Foi com Kotlin, uma linguagem versátil e poderosa, que dei os primeiros
+        passos nesse vasto campo do conhecimento. Desenhando algoritmos e
+        estruturas de dados no caderno, desenvolvi uma compreensão sólida dos
+        fundamentos da programação. À medida que o tempo passava, as
+        circunstâncias mudaram e finalmente tive acesso a um computador. Foi
+        nesse momento que transitei para o JavaScript, uma linguagem que me
+        permitiu expandir meus horizontes e criar aplicações completas, desde
+        simples páginas web até complexas APIs de autenticação.
+      </p>
+
+      <p>
+        {" "}
+        A transição para o mundo digital trouxe consigo desafios empolgantes,
+        mas também recompensas significativas. Hoje, posso afirmar com orgulho
+        que sou capaz de desenvolver soluções ponta a ponta, contribuindo para a
+        construção de aplicações que não apenas funcionam, mas também agregam
+        valor ao usuário.
+      </p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
 
 export default function Home() {
   return (
     <div>
       <SectionOne />
+      <SectionTwo />
     </div>
   );
 }
