ac0c9f701e8cf027dd3537569485dd34b59694e0
87tawanzinho <87tawanzinho@gmail.com>
2023-12-22 03:54:11 +0000

    port complete

---
+++ app/components/SectionThree.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+
+export default function SectionThree() {
+  return (
+    <div className="h-screen bg-three flex justify-center items-center flex-col">
+      <div className="z-10">
+        <p className="text-green-400 font-bold textShadow xs: text-5xl lg:text-7xl">
+          Meus Projetos
+        </p>
+      </div>
+
+      <div className="flex justify-center items-center gap-8 mt-10 flex-wrap ">
+        <div className="cursor-pointer">
+          <Link
+            href="https://blog-front-seven-lake.vercel.app/"
+            target="_blank"
+          >
+            <button className="bg-purple-400 rounded w-32 h-10 font-bold transition-all hover:bg-green-400 ">
+              Blog Pessoal
+            </button>
+          </Link>
+        </div>
+
+        <div className="cursor-pointer">
+          <Link href="https://workout-inky-delta.vercel.app/" target="_blank">
+            <button className="bg-purple-400 rounded w-32 h-10 font-bold transition-all hover:bg-green-400">
+              Rotina
+            </button>
+          </Link>
+        </div>
+
+        <div className="cursor-pointer">
+          <Link href="https://87tawan.github.io/Hamburgueria/" target="_blank">
+            <button className="bg-purple-400 rounded w-32 h-10 font-bold transition-all hover:bg-green-400">
+              Chefia Burguer
+            </button>
+          </Link>
+        </div>
+
+        <div className="cursor-pointer">
+          <Link href="https://tawanzinh9.github.io/lalaa/#/" target="_blank">
+            <button className="bg-purple-400 rounded w-32 h-10 font-bold transition-all hover:bg-green-400">
+              Tartooga
+            </button>
+          </Link>
+        </div>
+
+        <div className="cursor-pointer">
+          <Link href="https://camping-ruddy.vercel.app/" target="_blank">
+            <button className="bg-purple-400 rounded w-32 h-10 font-bold transition-all hover:bg-green-400">
+              Camping
+            </button>
+          </Link>
+        </div>
+
+        <div className="cursor-pointer">
+          <Link
+            href="https://tawanzinh9.github.io/WEBSITE-ZELDA-PC/"
+            target="_blank"
+          >
+            <button className="bg-purple-400 rounded w-32 h-10 font-bold transition-all hover:bg-green-400">
+              Zelda
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import SectionOne from "./components/SectionOne";
 import SectionTwo from "./components/SectionTwo";
+import SectionThree from "./components/SectionThree";
 
 export default function Home() {
   return (
     <div>
       <SectionOne />
       <SectionTwo />
+      <SectionThree />
     </div>
   );
 }
